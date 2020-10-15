114f51f71fe8491137ac7ecc3526c7d710965884
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-10-15 13:04:52 +0700

    Project 8

---
+++ src/PBO10119036Latihan8Pecahan.java
@@ -0,0 +1,24 @@
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * NAMA     : ALFI NURIZKYA
+ * KELAS    : PBO
+ * NIM      : 10119036
+ * DESKRIPSI PROGRAM : PERINTAH MEMASUKAN DATA INTEGER
+ */
+public class PBO10119036Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+    }
+    
+}
